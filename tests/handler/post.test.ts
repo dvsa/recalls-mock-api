f58fd41fe994b044c7e6827e787b83ad95297882
Alexis Corney <a.corney@kainos.com>
2024-07-17 11:59:16 +0100

    feat: add unit tests for all handlers

--- tests/handler/post.test.ts
+++ tests/handler/post.test.ts
@@ -1,12 +1,39 @@
 import type {
-  APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayEventRequestContext,
+  APIGatewayProxyEvent, APIGatewayProxyResult,
 } from 'aws-lambda';
-import { v4 } from 'uuid';
 import { handler } from '../../src/handler/post';
+import validUsageKey from '../../src/validator/apiUsageKey';
+import validAuthorisation from '../../src/validator/authorisation';
+import Vehicles from '../../src/data/vehicles';
+import { allRequiredFieldsCreateRecall, validDateFormat } from '../../src/validator/validatorsRecall';
+
+jest.mock('../../src/validator/authorisation.ts');
+jest.mock('../../src/validator/apiUsageKey.ts');
+jest.mock('../../src/validator/validatorsRecall');
+const mockAPIKeyValidator = jest.mocked(validUsageKey);
+const mockAuthValidator = jest.mocked(validAuthorisation);
+const mockDateValidator = jest.mocked(validDateFormat);
+const mockAllRequiredFieldsCreateRecall = jest.mocked(allRequiredFieldsCreateRecall);
+
+
+jest.mock('../../src/validator/validatorsRecall', () => {
+  return {
+    validDateFormat: jest.fn().mockImplementation(() => mockDateValidator ),
+    allRequiredFieldsCreateRecall: jest.fn().mockImplementation(() => mockAllRequiredFieldsCreateRecall ),
+  };
+});
 
 jest.mock('../../src/util/logger.ts');
 
 describe('Test Post Lambda Function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockAuthValidator.mockReturnValue(true);
+    mockAPIKeyValidator.mockReturnValue(true);
+    mockDateValidator .mockReturnValue(true);
+    mockAllRequiredFieldsCreateRecall.mockReturnValue(true);
+  });
+
   test('should return 201 with the body', async () => {
     const body: Record<string, string> = {
       vin: 'string',
@@ -14,16 +41,30 @@ describe('Test Post Lambda Function', () => {
       dvsaCampaignReference: 'R/2013/121',
       recallCampaignStartDate: '2022-01-31',
     };
-    const requestContext: APIGatewayEventRequestContext = <APIGatewayEventRequestContext> { requestId: v4() };
-    const headers: Record<string, string> = { Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ij', 'X-Api-Key' : 'theprovidedapiusagekey' };
+
     const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> {
       body: JSON.stringify(body),
-      requestContext,
-      headers,
     };
 
     const res: APIGatewayProxyResult = await handler(eventMock);
 
     expect(res.statusCode).toBe(201);
+  });
+
+  test('should return 409 if recall already registered', async () => {
+    const body: Record<string, string> = {
+      vin: Vehicles[0].vin,
+      manufacturerCampaignReference: Vehicles[0].manufacturerCampaignReference,
+      dvsaCampaignReference: Vehicles[0].dvsaCampaignReference,
+      recallCampaignStartDate: '2022-01-31',
+    };
+
+    const eventMock: APIGatewayProxyEvent = <APIGatewayProxyEvent> {
+      body: JSON.stringify(body),
+    };
+
+    const res: APIGatewayProxyResult = await handler(eventMock);
+
+    expect(res.statusCode).toBe(409);
   });
 });
